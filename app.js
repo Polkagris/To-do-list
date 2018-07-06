30f9c57d70edfc432991d64a890034b0d7fe2139
Arny <arne.n.bjerkaas@gmail.com>
2018-07-06 18:27:55 +0200

    change up grid to fit li's

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-
+const text = document.getElementById('input-text-field');
+const addToDo = document.getElementById('add-todo');
+console.log(addToDo);
+//add todo.innerHTML to variable
+//add todo button linked with eventListener click
+//when clicked => save text to variable
+//transfer value to tag element in ul
