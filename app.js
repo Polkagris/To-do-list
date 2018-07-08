67dbb81d13fd6ca28f9903228e455c05e34c65fd
Arny <arne.n.bjerkaas@gmail.com>
2018-07-08 12:14:36 +0200

    sort out grid containers - not finished

--- app.js
+++ app.js
@@ -7,20 +7,32 @@ addToDo.addEventListener("click", function(){
   var textValue = document.getElementById('input-text-field').value;
     if(textValue){
       createNewToDo(textValue);
-         console.log(textValue);
     }
+      text.value = "";
 });
 
+//creating new todo to the DOM
  function createNewToDo(textValue){
+   //creating text from input
    var parentDiv = document.getElementById('todo-container');
    var originalUl = document.getElementById('list');
    var newToDo = document.createElement('ul');
    newToDo.classList.add('list');
    newToDo.innerText = textValue;
 
-   //where the new list item will go in the DOM
+  //create remove button
+  var rmvButton = document.createElement('button');
+  rmvButton.classList.add('remove');
+  rmvButton.innerHTML = "Remove";
 
+  //create checked button
+  var cheButton = document.createElement('button');
+  cheButton.classList.add('checked');
+  cheButton.innerHTML = "Checked";
+   //where the new list-item will go in the DOM
    parentDiv.insertBefore(newToDo, originalUl);
+   newToDo.appendChild(rmvButton);
+   newToDo.appendChild(cheButton);
 
 
  }
